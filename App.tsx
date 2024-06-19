233fc497a803efbc98f98b4f03b1d8f8cc51656a
Dave Man <davephman@hkbh.org.hk>
2024-06-19 12:28:16 +0800

    add drawer, tab for android and ios

--- App.tsx
+++ App.tsx
@@ -18,44 +18,62 @@ const Stack = createNativeStackNavigator<RoutesParams>();
 const Tab = createBottomTabNavigator<Routes>();
 const Drawer = createDrawerNavigator<Routes>();
 
-export default function App(){
-  return(
-    // <NavigationContainer>
-    //   <Stack.Navigator>
-    //     <Stack.Screen name="Home" component={Home} />
-    //     <Stack.Screen
-    //         name="Details"
-    //         component={Details}
-    //         options={({ route }) => ({
-    //           headerRight: () => {
-    //               return (
-    //                   <Button
-    //                       title="Buy"
-    //                       onPress={() => {}}
-    //                       disabled={route.params.stock === 0}
-    //                   />
-    //               );
-    //           },
-    //         })}
-    //     />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-    <NavigationContainer>
-        {Platform.OS === "ios" && (
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={Home} />
-                <Tab.Screen name="News" component={News} />
-                <Tab.Screen name="Settings" component={Settings} />
-            </Tab.Navigator>
-        )}
 
-        {Platform.OS == "android" && (
-            <Drawer.Navigator>
-                <Drawer.Screen name="Home" component={Home} />
-                <Drawer.Screen name="News" component={News} />
-                <Drawer.Screen name="Settings" component={Settings} />
-            </Drawer.Navigator>
-        )}
-    </NavigationContainer>
-  );
+function TabNavigator() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="News" component={News} />
+            <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+    );
+}
+
+function IOSNavigator() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="News" component={News} />
+            <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
+    );
+}
+
+function AndroidNavigator() {
+    return (
+        <Drawer.Navigator>
+            <Drawer.Screen name="Tabs" component={TabNavigator} options={{ drawerLabel: 'Home' }} />
+            <Drawer.Screen name="Settings" component={Settings} />
+            <Drawer.Screen name="News" component={News} />
+        </Drawer.Navigator>
+    );
+}
+function StackNavigator() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={Platform.OS === "ios" ? IOSNavigator : AndroidNavigator} />
+            <Stack.Screen
+                name="Details"
+                component={Details}
+                options={({ route }) => ({
+                    headerRight: () => (
+                        <Button
+                            title="Buy"
+                            onPress={() => {}}
+                            disabled={route.params && route.params.stock === 0}
+                        />
+                    ),
+                })}
+            />
+        </Stack.Navigator>
+    );
+}
+
+
+export default function App(){
+    return (
+        <NavigationContainer>
+            <StackNavigator />
+        </NavigationContainer>
+    );
 }
